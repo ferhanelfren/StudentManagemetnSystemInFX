09f14e9f954c6c9eeff0261063a1cf53b25d7dc6
Kurimao-UMDC <ferhanelfren.mao@umindanao.edu.ph>
2026-04-11 15:31:51 +0800

    Initialized another branch for user registration design

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.studentmanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.studentmanagementsystem to javafx.fxml;
     exports com.mycompany.studentmanagementsystem;
