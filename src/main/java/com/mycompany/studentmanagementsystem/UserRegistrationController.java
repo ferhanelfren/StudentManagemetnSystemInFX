1c573b7ba50ffb8d69956ef0005c030830a4a3c4
Kurimao-UMDC <ferhanelfren.mao@umindanao.edu.ph>
2026-04-13 21:03:18 +0800

    still configuring on mysql-connector to this package.

--- src/main/java/com/mycompany/studentmanagementsystem/UserRegistrationController.java
+++ src/main/java/com/mycompany/studentmanagementsystem/UserRegistrationController.java
@@ -3,13 +3,34 @@ package com.mycompany.studentmanagementsystem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 
 public class UserRegistrationController implements Initializable {
+
+    @FXML
+    private Button nextButton;
+    @FXML
+    private Button cancelButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        Alert alrt = new Alert(Alert.AlertType.WARNING);
+        alrt.setTitle("Sample Only");
+        alrt.setContentText("Sampleing Only");
+        alrt.showAndWait();
+    }
+
+    @FXML
+    private void cancelButtonAction(ActionEvent event) {
+    }
     
 }
