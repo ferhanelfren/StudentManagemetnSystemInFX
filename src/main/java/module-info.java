d6fac68ca723af912d23c64256f07e6a90cae3fc
Kurimao-UMDC <ferhanelfren.mao@umindanao.edu.ph>
2026-04-14 20:33:03 +0800

    finished to stablish the connection of dependency of mysql-connector-j and registered to the module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.studentmanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.mycompany.studentmanagementsystem to javafx.fxml;
     exports com.mycompany.studentmanagementsystem;
